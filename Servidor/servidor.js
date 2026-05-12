ea87ba96ab213ebf2f4d0160446540b6ef5a0c64
caue <caueaquino09@gmail.com>
2026-05-11 21:15:50 -0300

    Criação do metodo busca , metodo critica

--- Servidor/servidor.js
+++ Servidor/servidor.js
@@ -10,7 +10,7 @@ const MongoClient = mongodb.MongoClient;
 const app = express();
 const server = http.createServer(app);
 
-
+//app.use('/paginas', express.static(path.join(__dirname, 'paginas')));
 // Servir arquivos estáticos (HTML, CSS, JS, imagens)
 app.use(express.static(path.join(__dirname, 'public')));
 //interpretar dados de formulario
@@ -83,6 +83,120 @@ app.post("/logar_usuario", async (req, res) => {
     return res.status(500).send("Erro no servidor");
      }
 });
+ app.post("/Escrever_Critica", (req, res) => {
+   const { resposta, arquivo } = req.body;
+ 
+   if (!resposta || !arquivo) {
+     return res.status(400).send("Resposta ou arquivo ausente.");
+   }
+ 
+   const caminhoArquivo = path.join(__dirname, 'public', arquivo);
+   console.log(caminhoArquivo);
+   fs.readFile(caminhoArquivo, 'utf8', (err, data) => {
+     if (err) {
+       console.error("Erro ao ler o arquivo:", err);
+       return res.status(500).send("Erro ao ler o arquivo.");
+     }
+ 
+     const marcador = "<!-- CRITICAS-AQUI -->";
+     const novaResposta = `
+          <div class="usuario">\n
+
+          <div class="avatar"><img src="https://i.pinimg.com/564x/c7/ab/cd/c7abcd3ce378191a3dddfa4cdb2be46f.jpg"></div>\n
+
+          <div class="conteudo-critica">\n
+            <h3>Título da Crítica</h3>\n
+            <p>${resposta}</p>\n
+          </div>\n
+          <h4>Escreva:</h4>\n
+          <form method="post" action="/Escrever_Resposta">\n
+          <textarea name="resposta" rows="4" cols="80" placeholder="Digite sua Resposta..."></textarea><br>\n
+          <input type="hidden" name="arquivo" value="hungergames.html">\n
+          <input type="submit" value="Enviar">\n
+          </form>\n
+        </div>\n
+
+        <div class="avaliacao">\n
+          Avaliação: ☆ ☆ ☆ ☆ ☆\n
+        </div>\n
+        <!-- RESPOSTAS-AQUI -->
+        `;  
+ 
+     if (!data.includes(marcador)) {
+       return res.status(500).send("Arquivo não possui marcador de respostas.");
+     }
+ 
+     const novoConteudo = data.replace(marcador, `${novaResposta}${marcador}`);
+ 
+     fs.writeFile(caminhoArquivo, novoConteudo, 'utf8', (err) => {
+       if (err) {
+         console.error("Erro ao salvar resposta:", err);
+         return res.status(500).send("Erro ao salvar resposta.");
+       }
+ 
+       res.redirect(`/${arquivo}`);
+     });
+   });
+ }); 
+
+  app.post("/Escrever_Resposta", (req, res) => {
+   const { resposta, arquivo } = req.body;
+ 
+   if (!resposta || !arquivo) {
+     return res.status(400).send("Resposta ou arquivo ausente.");
+   }
+ 
+   const caminhoArquivo = path.join(__dirname, 'public', arquivo);
+   console.log(caminhoArquivo);
+   fs.readFile(caminhoArquivo, 'utf8', (err, data) => {
+     if (err) {
+       console.error("Erro ao ler o arquivo:", err);
+       return res.status(500).send("Erro ao ler o arquivo.");
+     }
+ 
+     const marcador = "<!-- RESPOSTAS-AQUI -->";
+     const novaResposta = `
+          <div class="usuario">\n
+
+          <div class="avatar"><img src="https://i.pinimg.com/564x/c7/ab/cd/c7abcd3ce378191a3dddfa4cdb2be46f.jpg"></div>\n
+
+          <div class="conteudo-critica">\n
+            <h3>Título da Crítica</h3>\n
+            <p>${resposta}</p>\n
+          </div>\n
+          <h4>Escreva:</h4>\n
+          <form method="post" action="/Escrever_Resposta">\n
+          <textarea name="resposta" rows="4" cols="80" placeholder="Digite sua Resposta..."></textarea><br>\n
+          <input type="hidden" name="arquivo" value="hungergames.html">\n
+          <input type="submit" value="Enviar">\n
+          </form>\n
+        </div>\n
+
+        <div class="avaliacao">\n
+          Avaliação: ☆ ☆ ☆ ☆ ☆\n
+        </div>\n
+        
+        `;  
+ 
+     if (!data.includes(marcador)) {
+       return res.status(500).send("Arquivo não possui marcador de respostas.");
+     }
+ 
+     const novoConteudo = data.replace(marcador, `${novaResposta}${marcador}`);
+ 
+     fs.writeFile(caminhoArquivo, novoConteudo, 'utf8', (err) => {
+       if (err) {
+         console.error("Erro ao salvar resposta:", err);
+         return res.status(500).send("Erro ao salvar resposta.");
+       }
+ 
+       res.redirect(`/${arquivo}`);
+     });
+   });
+ }); 
+
+ 
+
 
 
 
